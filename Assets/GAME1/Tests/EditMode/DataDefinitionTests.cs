8c12f26725746b1b45893e7ef68ffeaa258011e6
mi <yyukis0904@gmail.com>
2026-09-23 03:46:17 +0900

    test(network): validate host authority rules

--- Assets/GAME1/Tests/EditMode/DataDefinitionTests.cs
+++ Assets/GAME1/Tests/EditMode/DataDefinitionTests.cs
@@ -1,6 +1,7 @@
 using Game1.Config;
 using Game1.Items;
 using Game1.Gameplay;
+using Game1.Network;
 using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
@@ -58,6 +59,21 @@ namespace Game1.Tests
 
             Object.DestroyImmediate(root);
             Object.DestroyImmediate(config);
+        }
+
+        [Test]
+        public void AuthorityRules_RejectDeadAndOutOfRangePlayers()
+        {
+            Assert.IsFalse(CoopAuthorityRules.CanInteract(PlayerLifeState.Dead, Vector3.zero, Vector3.zero, 3f));
+            Assert.IsFalse(CoopAuthorityRules.CanInteract(PlayerLifeState.Alive, Vector3.zero, Vector3.forward * 3.1f, 3f));
+            Assert.IsTrue(CoopAuthorityRules.CanInteract(PlayerLifeState.Alive, Vector3.zero, Vector3.forward * 3f, 3f));
+        }
+
+        [Test]
+        public void AuthorityRules_SharedCarryRequiresBothPlayersWithinTwoMeters()
+        {
+            Assert.IsTrue(CoopAuthorityRules.CanStartSharedCarry(Vector3.left * 0.5f, Vector3.right * 0.5f, Vector3.zero));
+            Assert.IsFalse(CoopAuthorityRules.CanStartSharedCarry(Vector3.left * 2f, Vector3.right * 2f, Vector3.zero));
         }
     }
 }
